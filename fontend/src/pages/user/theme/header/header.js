5536ccdf588cfdc9c0a2ed4810ff89edd19192a1
Akinoiii <tranhuyktvn27@gmail.com>
2024-09-07 01:11:40 +0700

    da css chua resposive

--- fontend/src/pages/user/theme/header/header.js
+++ fontend/src/pages/user/theme/header/header.js
@@ -6,12 +6,15 @@ import "./header.scss";
 
 const Header = () => {
   const [isFixed, setIsFixed] = useState(false);
+  const [showCustomService, setShowCustomService] = useState(true);
 
   const handleScroll = () => {
-    if (window.scrollY > 100) { // Điều chỉnh giá trị 100px tùy thuộc vào kích thước header của bạn
+    if (window.scrollY > 100) {
       setIsFixed(true);
+      setShowCustomService(false); // Ẩn custom-service khi cuộn qua 100px
     } else {
       setIsFixed(false);
+      setShowCustomService(true); // Hiện custom-service khi cuộn lại lên đầu trang
     }
   };
 
@@ -21,30 +24,39 @@ const Header = () => {
   }, []);
 
   return (
-    <div className={`header ${isFixed ? 'header-fixed' : ''}`}>
+    <div className="header">
+      <div className="banner">
+        <img
+          className="banner-img"
+          src="https://cocolux.com/storage/upload_image/images/Bao_bi_moi/VQMM/topbanner.gif"
+          alt="Logo"
+        />
+      </div>
       <div className="header-top">
         <div className="container">
           <FontAwesomeIcon icon={faPhoneVolume} />
           <span className="phone">0364670752</span>
         </div>
       </div>
-      <div className={`header-main ${isFixed ? 'hidden-header' : ''}`}>
-        <div className="container-header-main">
+      {/* Khi lăn màn hình, chỉ cố định logo-img, search-bar và shopping-cart */}
+      <div className={"header-main"}>
+        <div
+          className={`container-header-main ${isFixed ? "fixed-elements" : ""}`}
+        >
           <Link to="/">
             <img
               className="logo-img"
-              src="/assets/images/logo.png"
+              src={
+                isFixed
+                  ? "/assets/images/logo-fixed.png"
+                  : "/assets/images/logo.png"
+              } // Logo khác nhau khi lăn màn hình
               alt="logo"
             />
           </Link>
           <div className="search-bar">
             <form className="search">
-              <button
-                className="dropdown"
-                style={{ padding: "10px 20px", cursor: "pointer" }}
-              >
-                Tất cả
-              </button>
+              <button className="dropdown">Tất cả</button>
               <input
                 className="input-search"
                 placeholder="Tìm kiếm sản phẩm bạn mong muốn"
@@ -57,26 +69,24 @@ const Header = () => {
                 />
               </button>
             </form>
-            <Link className="no-underline">
+            <Link className="shopping-cart">
               <img
                 src="https://cocolux.com/images/cart-icon.svg"
                 alt="icon-shopping-cart"
               />
               <span>Giỏ hàng</span>
             </Link>
-            <Link className="no-underline">
-              <img
-                src="/assets/icons/hotline-icon.png"
-                alt="icon-hotline"
-              />
-              <span>Hỗ trợ khách hàng</span>
-            </Link>
+            {/* Ẩn custom-service khi cuộn qua header */}
+            {showCustomService && (
+              <Link className="custom-service">
+                <img src="/assets/icons/hotline-icon.png" alt="icon-hotline" />
+                <span>Hỗ trợ khách hàng</span>
+              </Link>
+            )}
           </div>
         </div>
       </div>
-      <div className="header-bottom">
-        {/* Nội dung phần dưới của header */}
-      </div>
+      <div className="header-bottom">{/* Nội dung phần dưới của header */}</div>
     </div>
   );
 };
